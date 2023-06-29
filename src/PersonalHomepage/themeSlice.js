2c6c3e0a0c456922fc3f0aa156d0cdd49d7c4d4f
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-29 18:27:36 +0200

    created themeSlice with toggleTheme action

---
+++ src/PersonalHomepage/themeSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    isTheme: false,
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      state.isTheme = !state.isTheme;
+    },
+  },
+});
+export const selectThemeState = (state) => state.isTheme;
+export const { toggleTheme } = themeSlice.actions;
+
+export const selectLightTheme = (state) => selectThemeState(state).isTheme;
+export default themeSlice.reducer;
