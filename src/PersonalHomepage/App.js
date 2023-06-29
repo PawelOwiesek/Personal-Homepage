a744cd9701d776b3ad68c0ce46117161106f00f4
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-29 18:30:33 +0200

    add providers

--- src/PersonalHomepage/App.js
+++ src/PersonalHomepage/App.js
@@ -1,8 +1,21 @@
-
+import { ThemeProvider } from "styled-components";
+import Header from "./common/Header";
+import { GlobalStyles } from "./GlobalStyles";
+import { useSelector } from "react-redux";
+import { selectThemeState } from "./themeSlice";
+import { darkTheme, lightTheme } from "./theme";
+import ThemeButton from "./common/ThemeSlider";
 
 function App() {
+  const isLigthTheme = useSelector(selectThemeState);
+
   return (
-    <div >
+    <div>
+      <ThemeProvider theme={isLigthTheme ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <ThemeButton />
+        <Header />
+      </ThemeProvider>
     </div>
   );
 }
