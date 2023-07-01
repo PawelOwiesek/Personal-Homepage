dac4b72e1a109c4e4d8f00178069784ddc97de40
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-01 16:41:06 +0200

    renamed button, updated path to myImage

--- src/PersonalHomepage/common/Header/index.js
+++ src/PersonalHomepage/common/Header/index.js
@@ -1,39 +1,37 @@
 import {
-  Description,
-  Envelope,
-  HeaderContainer,
   HeaderContent,
   HeaderImage,
-  Link,
-  MailButton,
+  HeaderContainer,
   PrevTitle,
   Title,
+  Description,
+  EmailButton,
+  Envelope,
+  Link,
 } from "./styled";
-import { myImage } from "../../Images/myImage";
+import { myImage } from "./myImage";
 import envelope from "../../Images/Message.svg";
 import { AboutMe } from "../../AboutMe";
 
-const Header = () => {
-  return (
-    <HeaderContainer>
-      <HeaderImage src={myImage} alt="myImage" />
-      <HeaderContent>
-        <PrevTitle>This is</PrevTitle>
-        <Title>{AboutMe.name}</Title>
-        <Description>{AboutMe.content}</Description>
-        <MailButton>
-          <Envelope src={envelope} alt="Email" />
-          <Link
-            href="mailto:{AboutMe.email}"
-            title="{AboutMe.EmailTitle}"
-            rel="noreferrer noopener"
-          >
-            Hire Me
-          </Link>
-        </MailButton>
-      </HeaderContent>
-    </HeaderContainer>
-  );
-};
+const Header = () => (
+  <HeaderContainer>
+    <HeaderImage src={myImage} alt="myImage" />
+    <HeaderContent>
+      <PrevTitle>This is</PrevTitle>
+      <Title>{AboutMe.name}</Title>
+      <Description>{AboutMe.content}</Description>
+      <EmailButton>
+        <Envelope src={envelope} alt="Email" />
+        <Link
+          href="mailto:{AboutMe.email}"
+          title="{AboutMe.EmailTitle}"
+          rel="noreferrer noopener"
+        >
+          Hire Me
+        </Link>
+      </EmailButton>
+    </HeaderContent>
+  </HeaderContainer>
+);
 
 export default Header;
