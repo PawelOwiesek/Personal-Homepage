b182888d18f9206d27274e197bc3d35437b5e1f5
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-01 16:56:54 +0200

    added path to themeButton background images

--- src/PersonalHomepage/theme.js
+++ src/PersonalHomepage/theme.js
@@ -1,41 +1,65 @@
-const mainColors = {
-    white: "#FFFFFF",
-    black: "#000000",
-    whiteLilac: "#FBFBFE",
-    slateGray: "#6E7E91",
-    scienceBlue: "#0366D6",
-    mineShaft: "#252525",
-  };
-  
-  export const lightTheme = {
-    basic: {
-      background: mainColors.whiteLilac,
-      mainText: mainColors.black,
-    },
-    section: {
-      background: mainColors.white,
-      divider: mainColors.slateGray,
-    },
-    buttons: {
-      background: mainColors.scienceBlue,
-      mainText: mainColors.white,
-    },
-  };
-  
-  export const darkTheme = {
-    basic: {
-      background: mainColors.black,
-      mainText: mainColors.white,
-    },
-    section: {
-      background: mainColors.mineShaft,
-      divider: mainColors.slateGray,
-    },
-    buttons: {
-      background: mainColors.scienceBlue,
-      mainText: mainColors.white,
-    },
-    themeButton:{
-        background: mainColors.slateGray
-    }
-  };
+import {
+  nightImage,
+  dayImage,
+} from "../PersonalHomepage/common/ThemeSlider/images";
+
+const colors = {
+  scienceBlue: "#0366D6",
+  slateGray: "#6E7E91",
+  white: "#FFFFFF",
+  black: "#000000",
+  whiteLilac: "#FBFBFE",
+  mineShaft: "#252525",
+  mercury: "#E5E5E5",
+  lightGray: "#363636",
+  iron: "#D1D5DA",
+  silver: "#7e7e7e",
+};
+
+export const lightTheme = {
+  basic: {
+    background: colors.whiteLilac,
+    text: colors.slateGray,
+  },
+
+  titles: {
+    color: colors.black,
+  },
+
+  buttons: {
+    background: colors.scienceBlue,
+    text: colors.white,
+  },
+
+  themeButton: {
+    backgroundImage: dayImage,
+  },
+
+  svgIcon: {
+    color: colors.white,
+  },
+};
+
+export const darkTheme = {
+  basic: {
+    background: colors.mineShaft,
+    text: colors.white,
+  },
+
+  titles: {
+    color: colors.white,
+  },
+
+  buttons: {
+    background: colors.scienceBlue,
+    text: colors.white,
+  },
+
+  themeButton: {
+    backgroundImage: nightImage,
+  },
+
+  svgIcon: {
+    color: colors.black,
+  },
+};
