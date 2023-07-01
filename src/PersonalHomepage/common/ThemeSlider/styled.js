d2dbae2a4110fd27f88861d640c933b09edc9f0b
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-01 15:46:18 +0200

    add bgr image to ThemeButton onchange theme

--- src/PersonalHomepage/common/ThemeSlider/styled.js
+++ src/PersonalHomepage/common/ThemeSlider/styled.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { ReactComponent as SunIcon } from "./images/sunIcon.svg";
 
 export const Wrapper = styled.div`
   display: flex;
@@ -6,15 +7,27 @@ export const Wrapper = styled.div`
   align-items: center;
   gap: 12px;
 `;
+
+export const TextContent = styled.p`
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 13px;
+  color: ${({ theme }) => theme.basic.text};
+  text-transform: uppercase;
+`;
+
 export const Button = styled.button`
   width: 48px;
   height: 26px;
-  border: 1px solid;
+  border: 1px solid ${({ theme }) => theme.basic.text};
   border-radius: 30px;
-  background-color: ${({ theme }) => theme.basic.background};
+  background-image: ${({ theme }) => theme.themeSwitcher.backgroundImage};
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
-export const Sun = styled.div`
+export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -22,12 +35,17 @@ export const Sun = styled.div`
   height: 20px;
   border: none;
   border-radius: 50%;
-  background-color: ${({ theme }) => theme.basic.background};
+  margin: 0 0 0 -3px;
+  background: ${({ theme }) => theme.basic.text};
+  transition: transform 0.3s;
+
+  ${({ slideRight }) =>
+    slideRight &&
+    css`
+      transform: translateX(20px);
+    `};
 `;
 
-export const ThemeText = styled.p`
-  font-weight: 700;
-  font-size: 12px;
-  line-height: 15.6px;
-  color: ${({ theme }) => theme.basic.background};
+export const Sun = styled(SunIcon)`
+  color: ${({ theme }) => theme.svgIcon.color};
 `;
