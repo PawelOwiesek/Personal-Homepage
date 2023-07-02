2d4f45a7ada96a5e22c89b590a6e91ec2f67a83c
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-02 12:44:17 +0200

    removed return

--- src/PersonalHomepage/common/SectionSkills/index.js
+++ src/PersonalHomepage/common/SectionSkills/index.js
@@ -1,17 +1,15 @@
 import { Container, Divider, List, SkillHeader, Item } from "./styled";
 
-const SkillSection = ({ title, content }) => {
-  return (
-    <Container>
-      <SkillHeader>{title}</SkillHeader>
-      <Divider />
-      <List>
-        {content.map((skill) => (
-          <Item key={skill}>{skill}</Item>
-        ))}
-      </List>
-    </Container>
-  );
-};
+const SkillSection = ({ title, content }) => (
+  <Container>
+    <SkillHeader>{title}</SkillHeader>
+    <Divider />
+    <List>
+      {content.map((skill) => (
+        <Item key={skill}>{skill}</Item>
+      ))}
+    </List>
+  </Container>
+);
 
 export default SkillSection;
