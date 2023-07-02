6627196e8a495f1dcee92028df7933302ca188d1
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-02 12:41:23 +0200

    created SkillSection component

---
+++ src/PersonalHomepage/common/SectionSkills/index.js
@@ -0,0 +1,17 @@
+import { Container, Divider, List, SkillHeader, Item } from "./styled";
+
+const SkillSection = ({ title, content }) => {
+  return (
+    <Container>
+      <SkillHeader>{title}</SkillHeader>
+      <Divider />
+      <List>
+        {content.map((skill) => (
+          <Item key={skill}>{skill}</Item>
+        ))}
+      </List>
+    </Container>
+  );
+};
+
+export default SkillSection;
