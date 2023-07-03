c985d88ab3cf17e549f16076cd78a256a944b2ef
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-03 19:32:06 +0200

    add sagaMiddleware

--- src/PersonalHomepage/store.js
+++ src/PersonalHomepage/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import themeReducer from "../PersonalHomepage/common/ThemeSlider/themeSlice";
+import { saveThemeSaga } from "./common/ThemeSlider/themeSaga";
 
-const store = configureStore({ reducer: themeReducer });
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
+  reducer: themeReducer,
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(saveThemeSaga);
 
 export default store;
