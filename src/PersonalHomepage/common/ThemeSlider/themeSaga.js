301f9232dc3747c053ee412b43972e7adddd1776
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-03 19:32:30 +0200

    created saga

--- src/PersonalHomepage/common/ThemeSlider/themeSaga.js
+++ src/PersonalHomepage/common/ThemeSlider/themeSaga.js
@@ -1,6 +1,6 @@
+import { select, call, takeEvery } from "redux-saga/effects";
 import { saveThemeInLocalStorage } from "./themeLocalStorage";
 import { selectThemeState } from "./themeSlice";
-import { select, call, takeEvery } from "redux-saga/effects";
 
 function* saveThemeHandler() {
   const setTheme = yield select(selectThemeState);
