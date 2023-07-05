e0abeb054121a641e841dacea51afe06c23c0acd
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-05 20:19:50 +0200

    added action as dependency

--- src/PersonalHomepage/common/ThemeSlider/themeSaga.js
+++ src/PersonalHomepage/common/ThemeSlider/themeSaga.js
@@ -1,6 +1,7 @@
 import { select, call, takeEvery } from "redux-saga/effects";
 import { saveThemeInLocalStorage } from "./themeLocalStorage";
 import { selectThemeState } from "./themeSlice";
+import { toggleTheme } from "./themeSlice";
 
 function* saveThemeHandler() {
   const setTheme = yield select(selectThemeState);
@@ -8,5 +9,5 @@ function* saveThemeHandler() {
 }
 
 export function* saveThemeSaga() {
-  yield takeEvery("*", saveThemeHandler);
+  yield takeEvery(toggleTheme.type, saveThemeHandler);
 }
