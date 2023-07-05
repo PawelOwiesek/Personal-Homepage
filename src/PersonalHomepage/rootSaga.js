f1afabeab613160e58737185f34185d65c8bd4b5
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-05 20:19:17 +0200

    removed themeSaga from rootSaga

--- src/PersonalHomepage/rootSaga.js
+++ src/PersonalHomepage/rootSaga.js
@@ -1,7 +1,6 @@
 import { all } from "redux-saga/effects";
-import { saveThemeSaga } from "../PersonalHomepage/common/ThemeSlider/themeSaga";
 import { gitHubSaga } from "../PersonalHomepage/features/Main/gitDataSaga";
 
 export function* rootSaga() {
-  yield all([saveThemeSaga(), gitHubSaga()]);
+  yield all([gitHubSaga()]);
 }
