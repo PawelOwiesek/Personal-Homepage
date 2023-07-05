3e535e6619efb6f9943981f2a88a9b6b0ccab30a
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-05 16:12:37 +0200

    add slice for repositories

---
+++ src/PersonalHomepage/features/Main/gitDataSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const gitDataSlice = createSlice({
+  name: "gitData",
+  initialState: {
+    status: "loading",
+    repositories: [],
+  },
+  reducers: {
+    fetchGitData: (state) => {
+      state.status = "loading";
+    },
+    fetchGitDataSuccess: (state, { payload: repositories }) => {
+      state.status = "success";
+      state.repositories = repositories;
+    },
+    fetchGitDataFailure: (state) => {
+      state.status = "failure";
+    },
+  },
+});
+
+export const { fetchGitData, fetchGitDataSuccess, fetchGitDataFailure } =
+  gitDataSlice.actions;
+
+export const selectDataState = (state) => state.gitData;
+export const selectRepositoresData = (state) =>
+  selectDataState(state).repositories;
+export default gitDataSlice.reducer;
