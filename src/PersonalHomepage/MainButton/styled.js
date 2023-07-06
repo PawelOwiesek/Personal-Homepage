e59575f55c16fff576c5c1be0a623f9996fa48a5
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-06 16:30:24 +0200

    moved styled Button highier in file hierarchy

---
+++ src/PersonalHomepage/MainButton/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: inline-flex;
+  padding: 12px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  background-color: ${({ theme }) => theme.buttons.background};
+  border: 1px solid ${({ theme }) => theme.divider.background};
+  border-radius: 4px;
+`;
