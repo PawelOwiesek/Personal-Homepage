1f37fcec09bc1ee7894d5c3341521d85c66de204
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-06 19:08:10 +0200

    created footer component

---
+++ src/PersonalHomepage/common/Footer/index.js
@@ -0,0 +1,15 @@
+import { Container } from "./styled";
+
+const Footer = () => (
+  <Container>
+    <p>Let's talk!</p>
+    <h3>PawelOwiesek@gmail.com</h3>
+    <p>
+      I’m always open to new projects whenever I have the time. If you have a
+      website, dashboard or mobile app in mind and need some help to make your
+      ideas come to life, feel free to conatct me
+    </p>
+  </Container>
+);
+
+export default Footer;
